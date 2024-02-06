5562925329d472ea131343c1c9898cb5096c217e
Wolfgang Vinatzer <44369896+GitOnMyLvl@users.noreply.github.com>
2024-02-06 16:12:36 +0100

    project setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,30 @@
 const path = require('path');
 const ESLintPlugin = require('eslint-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  stats: {
+    children: true,
+  },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
-  plugins: [new ESLintPlugin()]
+  plugins: [
+    new ESLintPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'To-Do-List',
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
