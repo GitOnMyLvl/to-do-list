053ae334f37c5d9effe34a6d58651d87dcabba32
Wolfgang Vinatzer <44369896+GitOnMyLvl@users.noreply.github.com>
2024-02-06 15:21:17 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [new ESLintPlugin()]
+};
