cc25f9b6fc4b0bbb88525f19d583011b308de161
Wolfgang Vinatzer <44369896+GitOnMyLvl@users.noreply.github.com>
2024-02-07 19:11:02 +0100

    additional setup and Storage class

---
+++ src/projects.js
@@ -0,0 +1,51 @@
+const tasks = [
+  {
+    id: 1,
+    title: 'Hello World',
+    todos: [
+      {
+        id: 1,
+        title: 'Hello',
+        description: 'World',
+        dueDate: '2100-09-20',
+        priority: 'Low',
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: 'To-Do-List',
+    todos: [
+      {
+        id: 1,
+        title: 'Create a new project',
+        description: 'Create a new project using webpack',
+        dueDate: '2025-09-20',
+        priority: 'High',
+      },
+      {
+        id: 2,
+        title: 'Add a new todo',
+        description: 'Add a new todo to the project',
+        dueDate: '2025-09-20',
+        priority: 'High',
+      },
+      {
+        id: 3,
+        title: 'Delete a todo',
+        description: 'Delete a todo from the project',
+        dueDate: '2025-09-20',
+        priority: 'High',
+      },
+      {
+        id: 4,
+        title: 'Edit a todo',
+        description: 'Edit a todo from the project',
+        dueDate: '2025-09-20',
+        priority: 'High',
+      },
+    ],
+  },
+];
+
+export default tasks;
